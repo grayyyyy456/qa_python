82120ed72b505f61539722cb583743fafa566185
Sergey Shiryev <grayyyyy456@yandex.ru>
2025-01-29 18:14:21 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # qa_python
+Заметки тестировщика
+Произведено по 1 проверки на каждый метод:
+1. test_add_new_book_add_two_books - проверяет, что книги добавляются в список
+2. test_set_and_get_book_genre - проверяет, что после жанр книге присваивается правильно
+3. test_only_set_book_genre - проверяет, что метод показывает жанр конкретной книги
+4. test_get_books_with_specific_genre_returns_correct_books - проверяет, что метод выводит список всех книг по конкретному жанру
+5. test_get_books_genre_returns_complete_dictionary - проверяет, что выводится весь спик добавленных книг
+6. test_get_books_for_children_returns_correct_children_books - проверяет, что в список попали книги для детей
+7. test_add_book_in_favorites - проверяет, что в избранное корректно добавляются книги
+8. test_delete_book_from_favorites - проверяет, что метод корректно удаляет книги из избранного по названию книги
+9. test_empty_list_of_favorites_books - проверяет, что метод показывает корректный список избранного
--- tests.py
+++ tests.py
@@ -18,7 +18,73 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_set_and_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и неукратимый питон')
+        collector.set_book_genre('Гарри Поттер и неукратимый питон', 'Ужасы')
+        assert collector.books_genre['Гарри Поттер и неукратимый питон'] == 'Ужасы'
+
+    def test_only_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и неукратимый питон')
+        collector.set_book_genre('Гарри Поттер и неукратимый питон', 'Ужасы')
+        assert 'Гарри Поттер и неукратимый питон' in collector.books_genre
+        assert collector.books_genre['Гарри Поттер и неукратимый питон'] == 'Ужасы'
+
+    def test_get_books_with_specific_genre_returns_correct_books(self):
+        collector = BooksCollector()
+        book_titles = [('Гарри Поттер и философский питон', 'Фантастика'), ('Мартин Иден', 'Комедии')]
+        for title, genre in book_titles:
+            collector.add_new_book(title)
+            collector.set_book_genre(title, genre)
+        fantasy_books = collector.get_books_with_specific_genre('Фантастика')
+        assert fantasy_books == ['Гарри Поттер и философский питон']
+
+        comedy_books = collector.get_books_with_specific_genre('Комедии')
+        assert comedy_books == ['Мартин Иден']
+
+    def test_get_books_genre_returns_complete_dictionary(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и неукратимый питон')
+        collector.set_book_genre('Гарри Поттер и неукратимый питон', 'Ужасы')
+        books_genre = collector.get_books_genre()
+        assert books_genre == {'Гарри Поттер и неукратимый питон': 'Ужасы'}
+
+    def test_get_books_for_children_returns_correct_children_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер и повелитель питонов')
+        collector.set_book_genre('Гарри Поттер и повелитель питонов', 'Мультфильмы')
+        children_books = collector.get_books_for_children()
+        assert children_books == ['Гарри Поттер и повелитель питонов']
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Мартин Иден')
+        collector.set_book_genre('Мартин Иден', 'Комедии')
+        collector.add_book_in_favorites('Мартин Иден')
+        favorites_books = collector.get_list_of_favorites_books()
+        assert favorites_books == ['Мартин Иден']
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Мартин Иден')
+        collector.set_book_genre('Мартин Иден', 'Комедии')
+        collector.add_book_in_favorites('Мартин Иден')
+        favorites_books = collector.get_list_of_favorites_books()
+        assert favorites_books == ['Мартин Иден']
+
+        collector.delete_book_from_favorites('Мартин Иден')
+        del_favorites_books = collector.get_list_of_favorites_books()
+        assert del_favorites_books == []
+
+    def test_empty_list_of_favorites_books(self):
+        collector = BooksCollector()
+        empty_favorites = collector.get_list_of_favorites_books()
+        assert empty_favorites == []
+
+
